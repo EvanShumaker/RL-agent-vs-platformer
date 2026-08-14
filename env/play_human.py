f7635a08f517327b1084146da3f24d5d6e5ed42a
Evan Shumaker <shumae1@unlv.nevada.edu>
2026-08-13 22:59:37 -0700

    template for the play_human file. Its what lets a person test or play the game

---
+++ env/play_human.py
@@ -0,0 +1,30 @@
+"""
+this file lets humans play the game and do manual testing of levels.
+Uses pygame for rendering and keyboard input, not used by RL agent
+"""
+
+import pygame
+import env.constants as C
+from env.game import Game
+
+KEY_TO_ACTION{
+
+}
+
+def get_action_from_keys():
+    """
+    Check what keys are being pressed and return that action
+    """
+
+
+
+def draw(screen, game):
+    """
+    used by pygame to draw the actual level
+    """
+
+
+def main():
+    pygame.init()
+
+
