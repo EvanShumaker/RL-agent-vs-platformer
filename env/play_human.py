027f97a2c1930c26657d882e79acd828ace2a258
Evan Shumaker <shumae1@unlv.nevada.edu>
2026-08-14 21:13:33 -0700

    draw function with dimensions and colors of goal, platforms, pit, and player

--- env/play_human.py
+++ env/play_human.py
@@ -32,6 +32,31 @@ def draw(screen, game):
     """
     screen.fill((30,30,40)) # dark background
 
+    state = game.get_state()
+
+    # now draw platforms using pos and dimensions. 
+    # draw.rect() takes screen, color, and pos / dimensions
+    for (x,y,w,h) in state["platforms"]:
+        pygame.draw.rect(screen, (100,100,110), (x,y,w,h))
+
+    # this is the goal
+    pygame.draw.rect(screen, (240,200,60), *state["goal"])
+
+    # Draw pit (visual of this helps with testing)
+    pxmin, pxmax = C.PIT_X_RANGE
+    pygame.draw.rect(screen, (60,20,20), (pxmin, C.SCREEN_HEIGHT - 10,pxmax - pxmin, 10))
+
+    # now for the player icon
+    if state["alive"]:
+        color = (80,180,240)
+    else:
+        color = (200,60,60)
+
+    pygame.draw.rect(screen,color, (state["player_x"], state["player_y"], 
+                                    C.PLAYER_WIDTH, C.PLAYER_HEIGHT) )
+    
+    pygame.display.flip()
+
 
 def main():
     pygame.init()
