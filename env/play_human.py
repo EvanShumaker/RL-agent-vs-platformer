766313e4e208389d95c22e5533ab59c3aaa8843b
Evan Shumaker <shumae1@unlv.nevada.edu>
2026-08-14 21:03:17 -0700

    get action from keys method

--- env/play_human.py
+++ env/play_human.py
@@ -7,14 +7,22 @@ import pygame
 import env.constants as C
 from env.game import Game
 
-KEY_TO_ACTION{
-
-}
 
 def get_action_from_keys():
     """
     Check what keys are being pressed and return that action
     """
+    keys = pygame.key.get_pressed()
+
+    # supporting wsad and arrows
+    if keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP]:
+        return C.ACTION_JUMP
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        return C.ACTION_LEFT
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        return C.ACTION_RIGHT
+
+    return C.ACTION_NOOP
 
 
 
@@ -22,6 +30,7 @@ def draw(screen, game):
     """
     used by pygame to draw the actual level
     """
+    screen.fill((30,30,40)) # dark background
 
 
 def main():
